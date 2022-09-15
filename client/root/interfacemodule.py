198ffa99d0ef440c33fa05693c52514f06e83dc8
Tekanse <superemailkonto@gmail.com>
2022-09-15 21:48:54 +0200

    Added missing part of instruction

--- client/root/interfacemodule.py
+++ client/root/interfacemodule.py
@@ -1,4 +1,35 @@
 #Under:
+import miniMap
+#Add:
+import uiminigameblacknwhite
+
+
+
+#Under:
+		self.dlgRefineNew.Hide()
+#Add:
+		self.dlgMinigameBnw = uiminigameblacknwhite.BlackNWhiteGamePage()
+		self.dlgMinigameBnw.Hide()
+
+
+
+#Under:
+		if self.dlgRefineNew:
+			self.dlgRefineNew.Destroy()
+#Add:
+		if self.dlgMinigameBnw:
+			self.dlgMinigameBnw.Destroy()
+
+
+
+#Under:
+		del self.dlgRefineNew
+#Add:
+		del self.dlgMinigameBnw
+
+
+
+#Under:
 	def AppendMaterialToRefineDialog(self, vnum, count):
 		self.dlgRefineNew.AppendMaterial(vnum, count)
 #Add:
